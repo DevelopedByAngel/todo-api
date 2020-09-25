9597d23074ebdab02dc1f7cad64c100a01aa8e8b
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2020-09-25 20:07:41 +0530

    intial

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+
 const express=require('express');
 const app=express();
 const cors=require('cors');
 const bodyParser=require('body-parser');
-const knex=require('knex')
+const knex=require('knex');
+const jwt=require('jsonwebtoken');
+const https=require('https');
+const http=require('http');
+const nodemailer=require('nodemailer');
+const dateformat=require('dateformat');
 const database=knex({
   client: 'pg',
   connection: {
@@ -10,6 +16,13 @@ const database=knex({
     user : 'postgres',
     password : 'angel@111@ps',
     database : 'spindle'
+  }
+});
+var mailtransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'remainderevent@gmail.com',
+    pass: 'remainder111'
   }
 });
 app.use(bodyParser.json());
@@ -22,66 +35,200 @@ app.get('/',(req,res)=>
 app.post('/login',(req,res)=>
 	{
 		const {email,password} = req.body;
-		var exists=false;
-		database.select('*').from('login').where('hash','=',email)
+		console.log('in'+email,password)
+		database.select('*').from('logind').where('email','=',email)
 		.then(data=>
 			{
 				console.log(data)
-				const valid=password===data[0].email;
+				const valid=password===jwt.verify(data[0].name,'spindle').password;
 				console.log(valid)
 				if (valid)
 				{
 					database.select('*').from('users').where('email','=',email)
 					.then(user=>
 					{	console.log(user)
-						exists=true;
-						res.json(user[0]);					
-					});
+						res.send(user[0]);					
+					})
 				}
 				else
 				{
-					res.json('wrong password');
+					rs.status(402).json('wrong password');
 				}
 			})
-		.catch(err=>res.status(400).json('unable to signin'))
-		
+		.catch(err=>res.status(400).json('error occured'))		
 	});
 app.post('/register', (req, res)=>
 	{
 		const {name,email,password} = req.body;
-		database.transaction(t=>
+		const hash=jwt.sign({password:password},'spindle');
+		database.transaction(trx=>
 		{
-			t.insert(
-			{
-				hash:password,
-				email:email,
-			})
-			.into('login')
-			.returning('email')
-			.then(email=>
-			{
-				console.log('login')
-				return t
-				.insert(
-				{
-					name:name,
-					email:email[0]
-				})
-				.into('users')
-				.returning('*')
-				.then(user=>
-				{
-					console.log('user')
-					if(user)
-						res.json(user[0]);
-				})
-
-			})
-			.then(t.commit)
-			.catch(err=>t.rollback)
+		trx('logind')
+		.insert(
+		{
+			name: hash,
+			email: email
 		})
-		.catch(err=>res.json(err))
+		.returning('email')
+		.then(loginemail=>
+		{
+			console.log('registerd with '+loginemail[0])
+			return trx('users')
+			.insert(
+			{
+				name:name,
+				email:loginemail[0]
+			})
+			.returning('*')
+			.then(user=>
+			{
+				console.log('logged in as '+user[0].name)
+				if(user[0])
+				{
+					database.select('*').from('users').where('email','=',user[0].email)
+					.then(usergot=>
+					{
+						// createTable(usergot[0].id)
+						var id=usergot[0].id;
+						const tablename='user'+id;
+		database.select('*').from('users').where('id','=',parseInt(id))
+		.then(user=>
+		{
+			if(user)
+			{
+				database.schema.createTable(tablename,(table)=>
+				{
+					table.increments('id');
+					table.string('task');
+					table.date('due');
+				})
+				.then(console.log('table created for'+user[0].name))
+				.catch(err=>console.log(err))
+			}
+		})
+					})
+					.catch(err=>Error('error'))
+					res.json(user[0])
+				}
+			})
+			.catch(err =>Error('error'))
+		})
+		.then(trx.commit)
+		.catch(trx.rollback);
+	})
+		.catch(err=>res.status(400).json(err))
 	});
+const createTable=(id)=>
+	{
+		const tablename='user'+id;
+		database.select('*').from('users').where('id','=',parseInt(id))
+		.then(user=>
+		{
+			if(user)
+			{
+				database.schema.createTable(tablename,(table)=>
+				{
+					table.increments('id');
+					table.string('task');
+					table.date('due');
+				})
+				.then(console.log('table created for'+user[0].name))
+				.catch(err=>console.log(err))
+			}
+		})
+	}
+app.post('/add',(req,res)=>
+	{
+		const {id,task,due}=req.body;
+		const tablename='user'+id;
+		database.insert(
+		{
+			task:task,
+			due:due
+		})
+		.returning('*')
+		.into(tablename)
+		.then(taskentered=>
+			{
+				if(taskentered)
+					getTasks(id,res);
+			})
+		.catch(err=>console.log(err))
+	});
+const getTasks=(id,res)=>
+{
+	var tasks;
+	database.select('*').from('user'+id)
+	.then(task=>
+	{
+		console.log(task)
+		res.json(task);
+	})
+}
+app.post('/tasks',(req,res)=>
+	{
+		const id = req.body.id;
+		console.log(id);
+		getTasks(id,res);
+	});
+app.post('/delete',(req,res)=>
+	{
+		const {id,taskid}=req.body;
+		database('user'+id).where('id','=',taskid)
+		.del()
+		.then(()=>
+		{
+			getTasks(id,res)
+		})
+	});
+app.post('/update',(req,res)=>
+	{
+		const {id,taskid,task}=req.body;
+		database('user'+id)
+		  .where({ id: taskid })
+		  .update({ task: task }, ['id', 'task'])
+		  .then(()=>
+		{
+			getTasks(id,res)
+		})
+	});
+database.select('*').from('users')
+.then(users=>
+	{
+		var users=users;
+		users.map(user=>
+	{
+		database.select('*').from('user'+user.id)
+		.then(tasks=>
+		{
+			tasks.map(task=>
+			{
+	
+				if(task.due.toString().slice(0,15)==dateformat(new Date()).slice(0,15))
+				{
+					mail(task.task,user.email);
+				}
+			})
+		})
+	})
+	})
+const mail=(task,mail)=>
+{
+	// console.log('mailed');
+	// var mailOptions = {
+	//   from: 'remainderevent@gmail.com',
+	//   to: mail,
+	//   subject:"Remainder : "+task,
+	//   html:  '<p> Today is the due date for <strong> "'+task+'" </strong>.Hope this remainder was helpful.<p><br><br><p>Add your tasks and due date <a href="#">here</a> to keep you remainded</p>'
+	// };
 
-app.listen(3000,()=>
-	{ console.log('ok')});
+	// mailtransport.sendMail(mailOptions, function(error, info){
+	//   if (error) {
+	//     console.log(error);
+	//   } else {
+	//     console.log('Email sent: ' + info.response);
+	//   }
+	// });
+	console.log(mail+" sent for "+task)
+}
+app.listen(3000);
